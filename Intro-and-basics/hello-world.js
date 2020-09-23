adaf4eb60d05a32e01aedb731b45fe71db4b79d7
Praveen Poonia <>
2020-09-23 18:14:16 +0530

    basic and types

---
+++ Intro-and-basics/hello-world.js
@@ -0,0 +1,6 @@
+// hello word function
+function sayHello(message) {
+    console.log(message);
+}
+var message = 'Hello everyone, Welcome in wolrd of typescript Programming';
+sayHello(message);
---
+++ Intro-and-basics/hello-world.ts
@@ -0,0 +1,8 @@
+// hello word function
+function sayHello(message) {
+  console.log(message);
+}
+
+const message = 'Hello everyone, Welcome in wolrd of typescript Programming';
+
+sayHello(message);
